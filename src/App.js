97e4a9b2451ab934b9d87379cc7808830423cafd
patilCDR <reshmaresh7862@gmail.com>
2023-04-10 16:18:08 +0530

    updated About

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './jsx/components/Home/Home';
-import About from './jsx/components/About';
-import Services from './jsx/components/Services';
-import Contact from './jsx/components/Contact';
-import Header from './jsx/layouts/Header';
-import Footer from './jsx/layouts/Footer';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./jsx/components/Home/Home";
+import About from "./jsx/components/About/About";
+import Services from "./jsx/components/Services";
+import Contact from "./jsx/components/Contact";
+import Header from "./jsx/layouts/Header";
+import Footer from "./jsx/layouts/Footer";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Header />
-        <div style={{ height: '70px' }}></div>
+        <div style={{ height: "70px" }}></div>
         <Routes>
-          <Route path='/' index element={<Home />} />
-          <Route path='about' index element={<About />} />
-          <Route path='services' index element={<Services />} />
-          <Route path='contact' index element={<Contact />} />
+          <Route path="/" index element={<Home />} />
+          <Route path="about" index element={<About />} />
+          <Route path="services" index element={<Services />} />
+          <Route path="contact" index element={<Contact />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,6 @@
-import aboutImg from './about.png';
-import teamImg from './team.jpg';
+import aboutImg from "./about.png";
+import teamImg from "./team.jpg";
+import MainImg from "./MainAbout.avif";
+import AB from "./Ab.jpg";
 
-export { aboutImg, teamImg };
+export { aboutImg, teamImg, MainImg, AB };
