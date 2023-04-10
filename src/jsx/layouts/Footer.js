f4040f0829f3bbaac66a7848c44c2f1d3b5f51b8
patilCDR <reshmaresh7862@gmail.com>
2023-04-11 01:12:51 +0530

    updated footer

--- src/jsx/layouts/Footer.js
+++ src/jsx/layouts/Footer.js
@@ -1,48 +1,42 @@
-import React from 'react';
-import { BsFacebook } from 'react-icons/bs';
+import React from "react";
 
 const Footer = () => {
   return (
     <div>
-      <footer className='bg-dark py-2'>
-        <div className='container'>
-          <div className='d-flex align-items-center justify-content-between flex-wrap h-100'>
-            <div className='my-auto d-flex align-items-center'>
-              <p className='text-white'>
-                Copyright &copy; 2023 All Rights Reserved by{' '}
-                <a href='http://www.bookxpert.co.in/' className='text-white'>
+      <footer className="bg-dark py-2">
+        <div className="container">
+          <div className="d-flex align-items-center justify-content-between flex-wrap h-100">
+            <div className="my-auto d-flex align-items-center">
+              <p className="text-white">
+                Copyright &copy; 2023 All Rights Reserved by
+                <a href="http://www.bookxpert.co.in/" className="text-white">
                   BookXpert
                 </a>
-                .
               </p>
             </div>
             <div>
-              <ul className='social-icons d-flex align-items-center'>
+              <ul className="social-icons d-flex align-items-center ">
                 <li>
-                  <a className='facebook' href='#'>
-                    <BsFacebook />
+                  <a className="facebook " href="#">
+                    <i class="bi bi-facebook fa-6x text-primary m-3"></i>
                   </a>
                 </li>
                 <li>
-                  <a className='facebook' href='#'>
-                    <BsFacebook />
-                  </a>
-                </li>
-                {/* <li>
-                  <a className='twitter' href='#'>
-                    <i className='fa fa-twitter'></i>
+                  <a className="instagram" href="#">
+                    <i class="bi bi-instagram fa-6x text-warning m-3"></i>
                   </a>
                 </li>
                 <li>
-                  <a className='dribbble' href='#'>
-                    <i className='fa fa-dribbble'></i>
+                  <a className="twitter" href="#">
+                    <i class="bi bi-twitter fa-6x text-primary m-3"></i>
                   </a>
                 </li>
+
                 <li>
-                  <a className='linkedin' href='#'>
-                    <i className='fa fa-linkedin'></i>
+                  <a className="youtube" href="#">
+                    <i class="bi bi-youtube fa-6x text-danger m-3"></i>
                   </a>
-                </li> */}
+                </li>
               </ul>
             </div>
           </div>
